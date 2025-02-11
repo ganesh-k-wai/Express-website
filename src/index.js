168cc3ce3f0abc48ed46a7c5d56e10d47aff924b
ganesh-k-wai <ganesh.kadam@waiin.com>
2025-02-11 16:07:22 +0000

    all basic setup done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 const express = require("express")
 const app = express()
+
+
+
 app.get('/', function (req, res) {
     res.send("hello world...!");
 })
+app.get('/about',function(req,res){
+    res.render("/about.ejs")
+})
+app.get('/gallery',function(req,res){
+    res.render("/gallery.ejs")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3000, function (req, res) {
     console.log("Connected to PORT:3000")
-})
+})  
